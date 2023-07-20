aa9702db387b9a097284756ca9d4ce0ef223680d
Sachindu Kavishka <cst21017@std.uwu.ac.lk>
2023-07-21 02:40:05 +0530

    myCommit

--- app/src/main/java/com/example/ridebuddy23/VehicleInformation.java
+++ app/src/main/java/com/example/ridebuddy23/VehicleInformation.java
@@ -1,8 +1,13 @@
 package com.example.ridebuddy23;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 public class VehicleInformation extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class VehicleInformation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vehicle_information);
+
+
+        LinearLayout linearLayout = findViewById(R.id.vehicle_layout);
+        CardView cardView = new CardView(this);
+
+        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams)cardView.getLayoutParams();
+        layoutParams.height = 100;
+        layoutParams.width = 100;
+
+        cardView.setLayoutParams(layoutParams);
+        cardView.setMinimumHeight(200);
+        linearLayout.addView(cardView);
+        System.out.println("Height" + cardView.getHeight());
+
     }
 }
